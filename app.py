d992488cac36f7a6ad8fcfd92a88fcead58180e1
Xavier <53248333+xxxavierlau@users.noreply.github.com>
2021-05-05 13:57:42 +0800

    Update app.py

--- app.py
+++ app.py
@@ -11,16 +11,5 @@ def render_index_page():
     return render_template('index.html', form=data, title=data.title)
 
 
-# @app.route('/chart')
-# def render_chart_page():
-#     pipeline = [{'$match': {'workarea_text': '深圳'}},
-#                 {'$group': {'_id': '$workyear', 'avg': {'$avg': '$providesalary_text'}}},
-#                 {'$sort': {'_id': 1}}]
-#     x = generate_list_for_matplotlib(get_data_from_mongodb(pipeline), '_id')
-#     y = generate_list_for_matplotlib(get_data_from_mongodb(pipeline), 'avg')
-#     imd = draw_chart('plot', x_asis=x, y_asis=y, x_label='Work years', y_label='Average salary')
-#     return render_template('register.html', img=imd)
-#
-
 if __name__ == '__main__':
     app.run()
